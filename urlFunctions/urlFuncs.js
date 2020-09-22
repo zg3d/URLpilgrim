d074493fc6858dd4ad69003200875c552f7a41a8
Devansh Shah <devzs6.98@gmail.com>
2020-09-22 16:06:18 -0400

    url function change async to sync

--- urlFunctions/urlFuncs.js
+++ urlFunctions/urlFuncs.js
@@ -15,38 +15,31 @@ const checkDuplicateUrl = (url, urlArray) => {
 };
 
 const testUrl = async (urlArray) => {
-  const protocolRegex = /^www(.+)/gi
-
-  for(let url of urlArray){
-    
-    if(protocolRegex.test(url)){
-      url = "https://" + url; 
+  const protocolRegex = /^www(.+)/gi;
+  const spinner = ora("URL PILGRIM - Checking URLs Status").start();
+  for (let url of urlArray) {
+    if (protocolRegex.test(url)) {
+      url = "https://" + url;
     }
 
-    const spinner = ora("URL PILGRIM - Checking URLs Status").start();
-    try{ 
-    const urlTest = await fetch(url,{method: "head", timeout: 1500});
-    spinner.stop();   
-     if(urlTest.status == 200){
-      printLog(`URL: ${url} Status: ${urlTest.status}`, "good");
-      }
-      else if(urlTest.status == 400 || urlTest.status == 404 ){
-      printLog(`URL: ${url} Status: ${urlTest.status}`, "bad");
-      }
-      else{
-      printLog(`URL: ${url} Status: Unknown`, "unknown");
-      } 
-    }catch (error) {
-      spinner.stop(); 
-      printLog(`URL: ${url} Status: Unknown`, "unknown");
-    } 
-    
-   };
-    
+    fetch(url, { method: "head" })
+      .then((urlTest) => {
+        if (urlTest.status == 200) {
+          printLog(`URL: ${url} Status: ${urlTest.status}`, "good");
+        } else if (urlTest.status == 400 || urlTest.status == 404) {
+          printLog(`URL: ${url} Status: ${urlTest.status}`, "bad");
+        } else {
+          printLog(`URL: ${url} Status: Unknown`, "unknown");
+        }
+      })
+      .catch((error) => {
+        printLog(`URL: ${url} Status: Unknown`, "unknown");
+      });
   }
-  
+  spinner.stop();
+};
 
 module.exports = {
-    checkDuplicateUrl,
-    testUrl
+  checkDuplicateUrl,
+  testUrl,
 };
